7080e12dbcb965984286b51989f53eb776bb9a1e
Dhruvil Shah <dsdruvil8@gmail.com>
2022-05-27 22:26:22 +0530

    fin v1

--- app/src/main/java/com/browserstack/webview/MainActivity.java
+++ app/src/main/java/com/browserstack/webview/MainActivity.java
@@ -1,17 +1,11 @@
 package com.browserstack.webview;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import im.delight.android.webview.AdvancedWebView;
 
-public class MainActivity extends AppCompatActivity implements AdvancedWebView.Listener {
-
-	private AdvancedWebView mWebView;
+public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,63 +13,10 @@ public class MainActivity extends AppCompatActivity implements AdvancedWebView.L
 		setContentView(R.layout.activity_main);
 
 		WebView.setWebContentsDebuggingEnabled(true);
-		mWebView = (AdvancedWebView) findViewById(R.id.webview);
+		WebView webview = (WebView) findViewById(R.id.webview);
 
-		mWebView.setListener(this, (AdvancedWebView.Listener) this);
-		mWebView.setMixedContentAllowed(false);
-		mWebView.loadUrl("https://browserstack.com");
+		webview.setWebViewClient(new WebViewClient());
+		webview.loadUrl("https://browserstack.com");
 
 	}
-
-	@SuppressLint("NewApi")
-	@Override
-	protected void onResume() {
-		super.onResume();
-		mWebView.onResume();
-		// ...
-	}
-
-	@SuppressLint("NewApi")
-	@Override
-	protected void onPause() {
-		mWebView.onPause();
-		// ...
-		super.onPause();
-	}
-
-	@Override
-	protected void onDestroy() {
-		mWebView.onDestroy();
-		// ...
-		super.onDestroy();
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		super.onActivityResult(requestCode, resultCode, intent);
-		mWebView.onActivityResult(requestCode, resultCode, intent);
-		// ...
-	}
-
-	@Override
-	public void onBackPressed() {
-		if (!mWebView.onBackPressed()) { return; }
-		// ...
-		super.onBackPressed();
-	}
-
-	@Override
-	public void onPageStarted(String url, Bitmap favicon) { }
-
-	@Override
-	public void onPageFinished(String url) { }
-
-	@Override
-	public void onPageError(int errorCode, String description, String failingUrl) { }
-
-	@Override
-	public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) { }
-
-	@Override
-	public void onExternalPageRequest(String url) { }
-}
+} 
